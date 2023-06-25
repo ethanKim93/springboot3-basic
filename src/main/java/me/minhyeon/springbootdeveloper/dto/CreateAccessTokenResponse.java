c46a945d8611230f05e6bb1d3c25f84bd5059144
ethanKim93 <wsmh.kim12@gmail.com>
2023-06-25 19:56:40 +0900

    feat : oauth 설정

---
+++ src/main/java/me/minhyeon/springbootdeveloper/dto/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package me.minhyeon.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
